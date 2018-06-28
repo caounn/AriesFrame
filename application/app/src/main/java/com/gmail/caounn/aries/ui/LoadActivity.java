1ac19f0dcddb611ac114ad26f9c2732c97d0a5cb
caounn <caounn@gmail.com>
2018-06-28 11:17:38 +0800

    基本Activity

---
+++ application/app/src/main/java/com/gmail/caounn/aries/ui/LoadActivity.java
@@ -0,0 +1,13 @@
+package com.gmail.caounn.aries.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import com.gmail.caounn.aries.R;
+
+public class LoadActivity extends AppCompatActivity {
+  @Override public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_load);
+  }
+}
