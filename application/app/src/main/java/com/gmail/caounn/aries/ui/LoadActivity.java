521fb685573482e7a25291fc6189f8eeb6abc37a
caounn <caounn@gmail.com>
2018-07-04 15:10:30 +0800

    修改继承关系

--- application/app/src/main/java/com/gmail/caounn/aries/ui/LoadActivity.java
+++ application/app/src/main/java/com/gmail/caounn/aries/ui/LoadActivity.java
@@ -2,10 +2,10 @@ package com.gmail.caounn.aries.ui;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import com.gmail.caounn.aries.R;
-import com.gmail.caounn.commons.ui.BaseActivity;
 
-public class LoadActivity extends BaseActivity {
+public class LoadActivity extends AppCompatActivity {
   @Override public void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_load);
--- application/app/src/main/java/com/gmail/caounn/aries/ui/MainActivity.java
+++ application/app/src/main/java/com/gmail/caounn/aries/ui/MainActivity.java
@@ -1,10 +1,10 @@
 package com.gmail.caounn.aries.ui;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import com.gmail.caounn.aries.R;
-import com.gmail.caounn.commons.ui.BaseActivity;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
