2114c551d24fb2eb16d2df64547f6bae779d9a16
caounn <caounn@gmail.com>
2018-07-11 17:13:38 +0800

    添加分享组件

--- application/app/src/main/java/com/gmail/caounn/aries/ui/LoadActivity.java
+++ application/app/src/main/java/com/gmail/caounn/aries/ui/LoadActivity.java
@@ -1,13 +1,42 @@
 package com.gmail.caounn.aries.ui;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 import com.gmail.caounn.aries.R;
 
 public class LoadActivity extends AppCompatActivity {
   @Override public void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_load);
+    TextView version = findViewById(R.id.version);
+    version.setText("model:" + Build.MODEL + "\r\n");
+    version.append("device:" + Build.DEVICE + "\r\n");
+    version.append("base_os:" + Build.VERSION.BASE_OS + "\r\n");
+    version.append("codeName:" + Build.VERSION.CODENAME + "\r\n");
+    version.append("incremental:" + Build.VERSION.INCREMENTAL + "\r\n");
+    version.append("release:" + Build.VERSION.RELEASE + "\r\n");
+    version.append("security_patch:" + Build.VERSION.SECURITY_PATCH + "\r\n");
+    version.append("sdk_int:" + Build.VERSION.SDK_INT + "\r\n");
+    version.append("id:" + Build.ID + "\r\n");
+    version.append("board:" + Build.BOARD + "\r\n");
+    version.append("product:" + Build.PRODUCT + "\r\n");
+    version.append("bootloader:" + Build.BOOTLOADER + "\r\n");
+    version.append("brand:" + Build.BRAND + "\r\n");
+    version.append("hardware:" + Build.HARDWARE + "\r\n");
+    version.append("display:" + Build.DISPLAY + "\r\n");
+    version.append("user:" + Build.USER + "\r\n");
+    version.append("type:" + Build.TYPE + "\r\n");
+    version.append("tags:" + Build.TAGS + "\r\n");
+    version.append("manufacturer:" + Build.MANUFACTURER + "\r\n");
+    version.append("host:" + Build.HOST + "\r\n");
+    version.append("fingerprint:" + Build.FINGERPRINT + "\r\n");
+
+
+
+
+
   }
 }
--- base/commons/src/main/java/com/gmail/caounn/commons/utils/SystemUtils.java
+++ base/commons/src/main/java/com/gmail/caounn/commons/utils/SystemUtils.java
@@ -1,4 +1,51 @@
 package com.gmail.caounn.commons.utils;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
+
 public class SystemUtils {
+  /**
+   * dp to px
+   */
+  public static float dp2px(Context context, float dpValue) {
+    float scale = context.getResources().getDisplayMetrics().density;
+    return dpValue * scale + 0.5f;
+  }
+
+  /**
+   * px to dp
+   */
+  public static float px2dp(Context context, float pxValue) {
+    float scale = context.getResources().getDisplayMetrics().density;
+    return pxValue / scale + 0.5f;
+  }
+
+  /**
+   * app version name
+   */
+  public static String getAppVersionName(Context context) {
+    try {
+      PackageInfo packageInfo =
+          context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+      return packageInfo.versionName;
+    } catch (PackageManager.NameNotFoundException e) {
+      return "未知版本";
+    }
+  }
+
+  /**
+   * system version
+   */
+  public static String getSystemVersion() {
+    return Build.VERSION.RELEASE;
+  }
+
+  /**
+   * os name
+   */
+  public static String getOsName() {
+    return Build.MODEL;
+  }
 }
