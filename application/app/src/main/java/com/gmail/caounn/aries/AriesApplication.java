a0ba09482a0fd7a0fc35496a6c62cedeea0832a9
caounn <caounn@gmail.com>
2018-07-11 16:17:41 +0800

    添加分享组件

---
+++ application/app/src/main/java/com/gmail/caounn/aries/AriesApplication.java
@@ -0,0 +1,9 @@
+package com.gmail.caounn.aries;
+
+import android.app.Application;
+
+public class AriesApplication extends Application {
+  @Override public void onCreate() {
+    super.onCreate();
+  }
+}
